f44b2e28c8e72f9465f442ae6a040e0e672ad7b9
SentexX <csipkekrisztian@gmail.com>
2022-10-30 22:14:16 +0100

    Feature - screenshot taking function added for bot

--- utils.py
+++ utils.py
@@ -2,7 +2,10 @@ import pyautogui
 from datetime import datetime
 
 
-def create_screenshot():
-    now = datetime.now()
+def create_screenshot(return_name=False):
+    now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     pic = pyautogui.screenshot()
-    pic.save('screenshots/screenshot'+now)
+    pic_name = 'screenshots/screenshot'+now
+    pic.save(pic_name)
+    if return_name:
+        return pic_name
