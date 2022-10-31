8226ca02fec550d82031f7275f255db42c8a8d00
SentexX <csipkekrisztian@gmail.com>
2022-10-31 14:45:56 +0100

    Bugfix: Screenshot save function fixed in utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def create_screenshot(return_name=False):
     now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     pic = pyautogui.screenshot()
-    pic_name = 'screenshots/screenshot'+now
-    pic.save(pic_name)
+    pic_name = 'screenshot'+now+".jpg"
+    pic.save('screenshots/'+pic_name)
     if return_name:
         return pic_name
